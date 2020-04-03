4514f1ae28448377e035e9b192c8d7ff6bf45edc
Zericy <58168039+Zericy@users.noreply.github.com>
2020-04-03 10:59:47 +0200

    Added StartScreen

---
+++ CrazyBrawler_MineralBrawlers/Assets/Essentials/Scripts/UI/StartScreenButtonBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreenButtonBehaviour : MonoBehaviour
+{
+    public void GoToScene(string SceneNameToLoad)
+    {
+        SceneManager.LoadScene(SceneNameToLoad);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
